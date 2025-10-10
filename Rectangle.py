c7bc67d052ce830198b034ddea39e4ddb7f58f38
Cleoven-Calderon <calderoncj58@gmail.com>
2025-10-10 11:13:07 +0800

    Add files via upload
    Rectangle program.

---
+++ Rectangle.py
@@ -0,0 +1,15 @@
+width = 35
+height = 7
+character = "*"
+
+for i in range(height):
+    print(character * width)
+
+#  GROUP 2
+#  CLEOVEN JEE P. CALDERON
+#  GLYSA DASOK
+#  JEROSE AMOMONPON
+#  ADRIAN CABAHUG
+#  KIEN ALIPAN
+#  RALPH ARCEL BOCATOT
+#  JOLIE ANN VENTIC
